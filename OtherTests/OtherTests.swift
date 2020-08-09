d8fb23b14168561000189a71533e79747ade16f3
Noah Gilmore <noah.w.gilmore@gmail.com>
2020-08-08 17:50:10 -0700

    Demonstration of setup that doesn't work, before the shared library creation

---
+++ OtherTests/OtherTests.swift
@@ -0,0 +1,16 @@
+//
+//  OtherTests.swift
+//  OtherTests
+//
+//  Created by Noah Gilmore on 8/8/20.
+//  Copyright © 2020 Noah Gilmore. All rights reserved.
+//
+
+import XCTest
+@testable import ExampleApp
+
+class OtherTests: XCTestCase {
+    func testExample() throws {
+        assertContainerWorks()
+    }
+}
